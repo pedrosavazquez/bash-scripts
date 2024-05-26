08fee95b320a765b3745503008d9ea3b03e26f93
codigomorrazo <codigomorrazo@gmail.com>
2024-05-26 09:26:26 +0200

    Increased temperature threshold

--- docker/bash/check-temperature.sh
+++ docker/bash/check-temperature.sh
@@ -8,7 +8,7 @@ VERSION="0.0.1\nScript to send with telegram CPU temperature"
 # Read cpu temperature using sensors
 CPU_TEMPERATURE=$(sensors | grep "Core 0" | awk '{print $3}' | cut -c 2- | tr -d °)
 CPU_TEMPERATURE_INT=$(echo $CPU_TEMPERATURE | tr -d .°C)
-if [ $CPU_TEMPERATURE_INT -gt 100 ]; then
+if [ $CPU_TEMPERATURE_INT -gt 700 ]; then
 	MESSAGE="Raspberry CPU temperature is too high: ${CPU_TEMPERATURE}"
 	# Send telegram message though sendMessage
 	/var/tmp/scripts/commands/sendMessage.sh "$MESSAGE"
